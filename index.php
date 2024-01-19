98ce70eb1bba53b6f12b9af9a200e26bd7d20f13
kevsiraki <kevsiraki@gmail.com>
2024-01-18 21:11:12 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LAMP Stack App</title>
+</head>
+<body>
+    <h1>Hello, LAMP Stack!</h1>
+    <p>This is a simple LAMP stack app without MySQL connections.</p>
+</body>
+</html>
